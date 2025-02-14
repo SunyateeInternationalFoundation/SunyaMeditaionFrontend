81ab044234cc166f32e74b3da7387c6145ba23ee
Kayathri-maths <kayualagarsamy@gmail.com>
2025-02-14 15:24:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-gray-600 font-bold text-center">
+        Sunya Meditation Website
+      </h1>
+    </>
+  );
+}
+
+export default App;
